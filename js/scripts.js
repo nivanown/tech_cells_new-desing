dcec9fcabd71cd1f3a832e32bc0a16b88a18539c
Nikita <32540777+nivanown@users.noreply.github.com>
2023-05-12 07:27:25 +0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var siteBtn = $('.menu-btn'),
 siteBtn.on('click', function(){
   $(this).toggleClass('open');
   siteMenu.toggleClass('show');
+  $('.header').toggleClass('active');
 });
 
 /*- color-btn -*/
@@ -111,4 +112,55 @@ accordion.forEach((element) => {
       panel.style.maxHeight = panel.scrollHeight + 'px';
     }
   });
+});
+
+/*- phone-input -*/
+[].forEach.call(document.querySelectorAll('input[type="tel"]'), function (input) {
+    let keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        let pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+1 (___) ___-____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            newValue = matrix.replace(/[_\d]/g, function (a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+            });
+        i = newValue.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            newValue = newValue.slice(0, i);
+        }
+        let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function (a) {
+                return "\\d{1," + a.length + "}";
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = newValue;
+        if (event.type == "blur" && this.value.length < 5) this.value = "";
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+});
+
+/*- type-file -*/
+$("input[type=file]").change(function (e) {
+  $(this).parents(".type-file").find(".type-file__text").text(e.target.files[0].name);
+});
+
+/*- modal -*/
+const myModal = new HystModal({
+    closeOnEsc: true,
+    backscroll: true,
+    afterClose: function(modal){
+        let videoframe = modal.openedWindow.querySelector('iframe');
+        if(videoframe){
+            videoframe.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+        }
+    },        
 });
